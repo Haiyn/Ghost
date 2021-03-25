489131b3cd58c6aec15828d4e8c1a0cb6763d45c
haiyn <corexaren@gmail.com>
2021-03-25 23:15:42 +0100

    feature #2

--- core/frontend/apps/private-blogging/index.js
+++ core/frontend/apps/private-blogging/index.js
@@ -33,6 +33,7 @@ module.exports = {
         let privateRoute = `/${PRIVATE_KEYWORD}/`;
         console.log("Test 1");
         console.log("Test 2");
+        console.log("Test 3");
 
         checkSubdir();
 
